8b93d8217154c6d9419a8c545df58f3758d31df2
Frappe Bench User <frappe@kanban-group.com>
2024-12-02 10:27:20 +0100

    First serious commit It's a baseline for experimenting with quotation web display. It's a gopy of the basic Frappe files with no modifications yet.

--- kanban_mods/hooks.py
+++ kanban_mods/hooks.py
@@ -19,7 +19,7 @@ email_brand_image = "/files/Kanban_logo_large.png"
 # Apps
 # ------------------
 
-# required_apps = []
+required_apps = ["erpnext"]
 
 # Each item in the list will be shown as an app in the apps page
 # add_to_apps_screen = [
