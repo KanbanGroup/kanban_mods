dba0d8fd6d83c1e2e8c74faf55bf22e1bedf7f8e
KanbanGroup <kevin@sound-and-spirit.nl>
2025-02-06 21:01:05 +0100

    Server-side bulk update prices from excel sheet done Next I need to work out a way to call it from the UI

--- kanban_mods/utils/buy_sell_utils/auto_update_prices.py
+++ kanban_mods/utils/buy_sell_utils/auto_update_prices.py
@@ -6,7 +6,13 @@ to insert new price_list items if they don't currently exist.
 '''
 import frappe
 from frappe import _
+from frappe.utils.xlsxutils import read_xlsx_file_from_attached_file as read_xlsx
+from frappe.utils.xlsxutils import read_xls_file_from_attached_file as read_xls
+
+import openpyxl as op
+
 from datetime import date
+
 from kanban_mods.utils.misc_functions import dump
 
 @frappe.whitelist() 
@@ -22,12 +28,11 @@ def update_prices(doctype, item_code, new_price):
             item = frappe.get_doc("Item", item_code) # the actual item itself
             new_price = float(new_price)
             update_buying_price(doctype, item_code, new_price, item) 
-            update_selling_price(doctype, item_code, new_price, item)
+#            update_selling_price(doctype, item_code, new_price, item)
             return "Success"
     except Exception as ex:
         return ex
         
-
 ##### The  next two functions are the primary actions #######
 
 def update_buying_price(doctype, item_code, price, item):
@@ -36,18 +41,21 @@ def update_buying_price(doctype, item_code, price, item):
     if buying_record:
         frappe.db.set_value(doctype, buying_record[0].name, 'price_list_rate', price) 
     else: 
-        create_item_price(doctype, item_code, 'Buying', price, item) 
+        price_list = item.brand+" Buying" 
+        create_item_price(doctype, item_code, price_list, price, item) 
+    frappe.db.commit()
     
 
 def update_selling_price(doctype, item_code, price, item): 
 
-    selling_price = get_selling_price(price, item)     
-    selling_record = frappe.get_all(doctype, filters={'item_code': item_code, 'selling': True}) 
-    if selling_record: 
-        frappe.db.set_value(doctype, selling_record[0].name, 'price_list_rate', selling_price) 
+    selling_record = frappe.get_all(doctype, filters={'item_code': item_code, 'selling': True})
+    if selling_record:
+        frappe.db.set_value(doctype, selling_record[0].name, 'price_list_rate', price) 
     else: 
-        create_item_price(doctype, item_code, 'Selling', selling_price, item) 
-            
+        price_list = item.brand+" Selling" 
+        create_item_price(doctype, item_code, price_list, price, item) 
+    frappe.db.commit()  
+
 ##### And here are the secondary, supportingfunctions #######
 
 def create_item_price(doctype, item_code, price_list, price, item):
@@ -69,9 +77,9 @@ def create_item_price(doctype, item_code, price_list, price, item):
                                     'valid_from': date.today()
                                     }).insert()
     
-
+####### This method is probably going to be redundant now ########
 def get_selling_price(price, item):
-    # go fetch the correct rule ... you need the Buying rule data
+    # # go fetch the correct rule ... you need the Buying rule data
     rule = frappe.get_doc("Pricing Rule", "Buying", {"item_group": item.item_group})
 
     margin = rule.margin_rate_or_amount
@@ -85,3 +93,99 @@ def get_selling_price(price, item):
         new_s_p= price + margin
     return new_s_p
 
+@frappe.whitelist()
+def bulk_insert():
+    # The idea here is to create buying and selling Item Price records for every variant
+    # This is just the driver for the actual action functions
+    manufs = frappe.get_all("Manufacturer")
+    filter = tuple(item.name for item in manufs)
+    items = frappe.get_all("Item")
+    ix = 0
+    ix2 = 0
+    for item in items:
+        if item.name.endswith(filter):
+            it = frappe.db.get_value("Item", item.name, ["item_name", "stock_uom", "brand", "item_group"], as_dict=1)
+            if it.brand:
+                update_buying_price("Item Price", it.item_name, 0, it)
+                update_selling_price("Item Price", it.item_name, 0, it)
+                ix += 1
+                print(ix2," -- ",ix)
+            else:
+                raise Exception("Stock Item "+item.name+" does not have a brand. Please fix this asap")
+        if ix == 100:
+            ix = 0
+            ix2 += 100
+            frappe.db.commit()
+
+    return "Its all ready for checking"
+
+@frappe.whitelist()
+def bulk_update_from_xls(file_name):
+    types = ('XLS', 'XLSX')
+    file_meta = frappe.get_value("File", {'file_name': file_name}, ['file_name', 'file_type'], as_dict=1)
+
+    if file_meta and file_meta.file_type in types:
+        # we have a valid file so lets process the data
+        parsed_sheet = parse_workbook(file_name)
+        dump(parsed_sheet)
+        for row in parsed_sheet['pricelist']:
+            # We are assuming the sheet has been normalised efore submission
+            # So we don't need to mess around working out which column holds
+            # which values - Item_name, Buying Price, Selling Price (optional)
+            item_code = row[0]
+            bp = float(row[1])
+            if len(row) > 2: 
+                sp = float(row[2])
+            else:
+                sp = bp
+            print(item_code, bp, sp)
+            item = frappe.db.get_value("Item", item_code, ["item_name", "stock_uom", "brand", "item_group"], as_dict=1)
+            update_buying_price("Item Price", item_code, bp, item)
+            update_selling_price("Item Price", item_code, sp, item)
+            
+        return " go check the results ... "
+    else:
+        # return an error message
+        return "Sorry but either " + file_name +" is not a valid Excel spreadsheet/workbook, or else it does not exist in the file system"
+    
+def parse_workbook(filename):
+    _file = frappe.get_doc("File", {"file_name": filename})
+    path_to_file = _file.get_full_path()
+    book = op.load_workbook(filename=path_to_file, data_only=True, read_only=False)
+
+    # Get the column titles
+    column = 0
+    col_titles = []
+    current_sheet = book[book.sheetnames[0]]
+    for col in current_sheet.iter_cols(1, current_sheet.max_column):
+        column += 1
+        col_titles.insert(column, col[0].value)
+
+    # Get the actual data
+    current_row = 0
+    new_prices = []    
+    current_row = 1    
+    for sheet in book.sheetnames:
+        current_sheet = book[sheet]
+        # Iterate the loop to read the cell values
+        for row in range(1, current_sheet.max_row):
+            price_row = []
+            column = 0
+            for col in current_sheet.iter_cols(1, current_sheet.max_column):
+                column += 1
+                if column == 1:
+                    price_row.insert(column, col[row].value+" - "+sheet)
+                else:
+                     price_row.insert(column, col[row].value)
+            new_prices.insert(current_row, price_row)
+            current_row += 1
+    parsed_sheet = {}
+    parsed_sheet['tites'] = col_titles
+    parsed_sheet['pricelist'] = new_prices
+    return parsed_sheet
+
+        
+    
+
+
+        
