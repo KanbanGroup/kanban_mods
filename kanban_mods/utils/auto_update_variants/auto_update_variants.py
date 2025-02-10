425a513f7133b07a448a53a30442987d21c72557
KanbanGroup <kevin@sound-and-spirit.nl>
2025-02-10 16:13:24 +0100

    Added a function to bulk add a single manufacturer suffix to stock items in the item list

--- kanban_mods/utils/auto_update_variants/auto_update_variants.py
+++ kanban_mods/utils/auto_update_variants/auto_update_variants.py
@@ -6,20 +6,24 @@ from erpnext.controllers.item_variant import get_variant
 
 @frappe.whitelist() 
 def update_variants():
+    
     manufs = frappe.get_all("Manufacturer")
     filter = tuple(item.name for item in manufs)
     items = frappe.get_all("Item")
+
     ix = 0
     ix2 = 0
     for item in items:
         if not item.name.endswith(filter) and not item.name == "Shipping":
             process_item(item, filter)
+            exit()
             ix += 1
             print(ix2," -- ",ix)
         if ix == 100:
             ix = 0
             ix2 += 100
             frappe.db.commit()
+            exit()
     return "Its all ready for checking"
     
 # create the variants of the template item
@@ -27,7 +31,7 @@ def process_item(item, filter):
     # first retrieve the item 
     template = frappe.get_doc("Item", item.name)
     # and check whether it is already a template (has_variants = true) 
-    # if necessary sure the item is defined as a template and saved
+    # if necessary make sure the item is defined as a template and saved
     if not template.has_variants:
         template.has_variants = 1
         template.save()
@@ -44,3 +48,20 @@ def process_item(item, filter):
                 ignore_permissions=True, # ignore write permissions during insert
             )
 
+def add_manufacturer_suffix(manufacturer = None):
+    if manufacturer: 
+        items = frappe.get_all("Item", filters = {'has_variants': True})
+        ix = 0
+        ix2 = 0
+        for item in items:
+            process_item(item, [manufacturer])
+            ix += 1
+            print(ix2," -- ",ix)
+            if ix == 100:
+                frappe.db.commit()
+                ix = 0
+                ix2 += 100
+                
+        return "Its all ready for checking" 
+    else:
+        return "You need to provide a manufacturer ID like SKF oe GENERIC"
